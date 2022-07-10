0fcda77f4e41df07293825e6753561c5731963dd
sundowns <tom@smallridge.com.au>
2022-07-10 14:49:25 +0800

    hand movement

--- player/Hand.gd
+++ player/Hand.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
-export(float) var max_distance_from_player: float = 2.0
+export(float) var max_distance_from_player: float = 16.0
 
-func _physics_process(delta):
-	pass	
+func update_hand_position(mouse_global_position: Vector2):
+	var parent_position = get_parent().global_transform.origin
+	var to_mouse = (mouse_global_position - parent_position)
+	position = to_mouse.normalized() *  min(to_mouse.length(), max_distance_from_player)
+#	print(parent_position, mouse_global_position, to_mouse)
+#	pass
 
-#func update_hand_position():
-#	var mouse_position = 
+func _input(event):
+	if event is InputEventMouseMotion:
+		update_hand_position(get_global_mouse_position())
